43ca4b45d6205ab225b99ba1bacd429c9ebcc96e
Parag18081994 <paragnaik2021.git@gmail.com>
2023-03-20 19:45:25 +0530

    First version

---
+++ src/components/intro/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.scss";
+import cloud from "../../images/cloud.png";
+import cloudSoft from "../../images/cloud-soft.png";
+import Navigation from "./navigation-bar";
+
+export const Intro = () => {
+    return (
+        <div className="Intro-section">
+            <div
+                className="vector-bg"
+                id="parallax"
+            ></div>
+            <img
+                src={cloud}
+                className="cloud"
+            />
+            <img
+                src={cloudSoft}
+                className="cloud-soft"
+            />
+
+            <div className="content">
+              <Navigation/>
+            </div>
+
+        </div>
+    );
+};
+
+export default Intro;
